5d466c562782caf9c7a40089062c24c2e185524a
AndresMate <andres.mateus01@uptc.edu.co>
2024-10-16 17:31:57 -0500

    fix

--- routes/cars.js
+++ routes/cars.js
@@ -3,14 +3,12 @@ import fetch from 'node-fetch';
 
 const router = express.Router();
 
-router.get('/cars', async(req, res)=>{
+router.get('/cars', async (req, res) => {
     try {
-    const response = await fetch('http://158.247.122.111:3000/car');
-        
+        const response = await fetch('http://localhost:3000/car');
         const data = await response.json();
         res.json(data.data);
         console.log(data.data);
-
     } catch (error) {
         res.status(500).json({ message: 'Error fetching characters' });
     }
