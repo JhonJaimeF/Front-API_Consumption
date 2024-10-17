1503c9baefb628007ea733ffbac2aea391bd1db1
AndresMate <andres.mateus01@uptc.edu.co>
2024-10-16 21:34:59 -0500

    peticiones

--- public/script-coches.js
+++ public/script-coches.js
@@ -1,5 +1,5 @@
 (() => {
-    fetch('http://localhost:3000/')
+    fetch('http://158.247.122.111:3000/car')
         .then(data => data.json())
         .then(data => console.log(data))
         .catch(err => console.log(err));
